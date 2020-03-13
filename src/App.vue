b4df3ef48051c8743d82c441845ff31f5d493436
Ssnnaaiill <newdk3025@gmail.com>
2020-03-13 17:54:12 +0900

    💄 Add buefy library to project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">🐈CAT FACTS💞</b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item href="/">Home</b-navbar-item>
+        <b-navbar-item href="#">Documentation</b-navbar-item>
+        <b-navbar-dropdown label="Info">
+          <b-navbar-item tag="router-link" :to="{path:'/about'}">About</b-navbar-item>
+          <b-navbar-item href="#">Contact</b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">Log in</a>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+    <router-view />
   </div>
 </template>
 
